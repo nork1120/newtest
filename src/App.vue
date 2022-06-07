b2c307b5eb233666ea5644c170d24f5fdd0375b6
nork1120 <nork1120@gmail.com>
2022-06-08 04:49:16 +0800

    第一次

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <TooSt></TooSt>
+  <Asdd></Asdd>
   <router-view/>
 </template>
-
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "./assets/all";
 </style>
+
+<script>
+import EmiTter from "../src/methods/mitt.js";
+import TooSt from '../src/components/TooSt.vue'
+import Asdd from '../src/components/BioTe.vue';
+export default {
+    provide(){
+    return {
+      EmiTter
+    }
+  },
+  components:{
+      TooSt,
+      Asdd
+    }
+}
+</script>
